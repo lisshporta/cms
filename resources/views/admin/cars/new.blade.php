dc89148b0d294c3fc99a8cbf4f67e5f65513e1ef
heyjordn <proxima.aust@gmail.com>
2022-08-15 19:50:05 -0500

    fix use the correct wording for Vehicles

--- resources/views/admin/cars/new.blade.php
+++ resources/views/admin/cars/new.blade.php
@@ -1,8 +1,8 @@
-@section('title', 'New Car')
+@section('title', 'New Vehicle')
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            New Car
+            New Vehicle
         </h2>
     </x-slot>
 
