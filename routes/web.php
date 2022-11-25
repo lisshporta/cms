adf72b7da1a53e720577a794b61bd3e9838a86b4
heyjordn <heyjordn@users.noreply.github.com>
2022-11-25 15:53:54 +0000

    Ran Pint to fix styling

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
 Route::get('/', ExplorePage::class)->name('home');
 Route::get('/vehicles/{slug}', [VehicleController::class, 'index'])->name('vehicle.show');
 
