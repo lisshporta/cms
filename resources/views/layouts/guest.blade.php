92e932e61b3b0ad808d86fd51715f7cd74fd9fe5
heyjordn <proxima.aust@gmail.com>
2023-05-09 12:49:02 -0500

    feat, improve SEO with  proper titles

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title', '/') - {{ config('app.name', 'Laravel') }}</title>
+        <title>Volt Auto Sales - Buy and Sell Cars in the Caribbean @yield('title', '/')</title>
 
         <!-- Scripts -->
         @vite(['resources/js/app.js','resources/css/app.css'])
--- resources/views/layouts/public/main.blade.php
+++ resources/views/layouts/public/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', '/') - {{ config('app.name', 'Volt') }}</title>
+    <title>Volt Auto Sales - Buy and Sell Cars in the Caribbean @yield('title', '/')</title>
 
     @livewireStyles
 
